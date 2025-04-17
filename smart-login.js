6f1ce324c6c87a83b65244776b0f63f749043e1f
sasaamragy <103282213+sasaamragy@users.noreply.github.com>
2025-04-17 05:39:18 +0200

    Fix redirect_uri for Netlify deployment

--- smart-login.js
+++ smart-login.js
@@ -2,6 +2,6 @@ function startSmartLogin() {
   FHIR.oauth2.authorize({
     client_id: "my-web-app", // ده اسم افتراضي في SMART Launcher، ممكن تغيريه لو سجلتي اسم مختلف
     scope: "launch openid fhirUser patient/*.read",
-    redirect_uri: "https://sasaamragy.github.io/curahub/redirect.html"
+    redirect_uri: "https://super-gelato-784bc7.netlify.app/redirect.html"
   });
 }
