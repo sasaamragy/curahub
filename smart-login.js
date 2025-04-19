245c69cc8e3df2e6a35ed1808125bf6e10698964
Sara A. <SASA_AMRAGY@YAHOO.COM>
2025-04-19 22:54:16 +0200

    correction of FHIR login

--- smart-login.js
+++ smart-login.js
@@ -1,10 +1,8 @@
-function startSmartLogin() {
-  const serviceUrl = "https://launch.smarthealthit.org/v/r4/sim/WzMsIiIsIiIsIkFVVE8iLDAsMCwwLCJsYXVuY2ggb3BlbmlkIGZoaXJVc2VyIHBhdGllbnQvKi5yZWFkIiwiaHR0cHM6Ly9zdXBlci1nZWxhdG8tNzg0YmM3Lm5ldGxpZnkuYXBwL3JlZGlyZWN0Lmh0bWwiLCIiLCIiLCIiLCIiLCIiLDAsMiwiIl0/fhir";
 
-  FHIR.oauth2.authorize({
-    client_id: "my-web-app", // ده اسمك اللي استخدمته في SMART Launcher
-    scope: "launch openid fhirUser patient/*.read",
-    redirect_uri: "https://super-gelato-784bc7.netlify.app/redirect.html",
-    iss: serviceUrl
-  });
-}
+FHIR.oauth2.authorize({
+  client_id: "curahub-client-app",
+  scope: "launch openid fhirUser patient/*.read",
+  redirect_uri: "https://sasaamragy.github.io/curahub/redirect.html",
+  iss: "https://launch.smarthealthit.org/v/r4",
+  pkce: true
+});
