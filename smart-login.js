f5665b428dd1e21a675d875a448e43f5f0f94829
Sara A. <SASA_AMRAGY@YAHOO.COM>
2025-04-19 23:25:09 +0200

    Fix redirect login

--- smart-login.js
+++ smart-login.js
@@ -3,6 +3,6 @@ FHIR.oauth2.authorize({
   client_id: "curahub-client-app",
   scope: "launch openid fhirUser patient/*.read",
   redirect_uri: "https://sasaamragy.github.io/curahub/redirect.html",
-  iss: "https://launch.smarthealthit.org/v/r4",
+ iss: "https://launch.smarthealthit.org/v/r4",
   pkce: true
 });
