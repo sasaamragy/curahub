1127e4bea1f637262bf71dba47d56aff922bae79
Sara A. <SASA_AMRAGY@YAHOO.COM>
2025-04-21 21:33:47 +0200

    تصحيحي redirect

--- smart-login.js
+++ smart-login.js
@@ -1,8 +1,8 @@
 
 FHIR.oauth2.authorize({
-  clientId: "curahub-client-app", // ✅ استخدم clientId بدل client_id
+  clientId: "curahub-client-app",
   scope: "launch openid fhirUser patient/*.read",
-  redirectUri: "https://sasaamragy.github.io/curahub/redirect.html", // ✅ استخدم camelCase: redirectUri
+  redirectUri: "https://sasaamragy.github.io/curahub/redirect.html",
   iss: "https://launch.smarthealthit.org/v/r4",
   pkce: true
 });
